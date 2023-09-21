333c6f562cc0c071fa2ddce09a5127eec31f9630
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-09-21 14:26:08 -0600

    downloaded pg package

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db/db-connection');
+const { application } = require('express');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 
 //set up the port for the server
@@ -14,10 +17,33 @@ app.get('/api', (req, res) => {
 });
 
 //get all the species
-app.get('/species', (req, res) => {
-    const species={species: "Bears"}
-    res.json({ species});
+app.get('/api/species', async (req, res) => {
+    try {
+        const results = await db.query('SELECT * FROM species');
+        res.status(200).json({
+            status: 'success',
+            results: results.rows.length,
+            data: {
+                species: results.rows,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
+
+//get sightings
+app.get("/api/sightings", async (req, res) => {
+    try {
+      const allSightings = await db.query(
+        "SELECT sightings.sighting_id, sightings.date_time as last_seen, sightings.healthy, sightings.location, individuals.nick_name as name, species.common_name, species.scientific_name FROM sightings LEFT JOIN individuals ON individuals.individual_id = sightings.individual_id LEFT JOIN species ON species.species_id = individuals.species_id"
+      );
+      res.json(allSightings.rows);
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+
 
 
 //listen to the port
